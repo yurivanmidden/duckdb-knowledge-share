a6902e196aec767e991237e5a31ea1147f47e67f
Yuri van Midden <yvanmidden@Carthago-MacBook-Pro-van-Yuri.local>
2026-03-19 11:49:10 +0100

    Knowledge sharing preparation

---
+++ kennisdeling.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6d557d929b4d5949b378668472137bbefd85996f5ea4a89e5aa13f4a10c97693
+size 12954
---
+++ main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a0432019df1c327e20339ef254a3097ef985ab797b2dce20f5dc49d617e4bbc6
+size 86
